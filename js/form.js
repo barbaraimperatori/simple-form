0080aaa1eb56893d0aa21e765567224e8c12ce9a
Barbara Imperatori <barbarabelen.i@gmail.com>
2019-03-06 23:44:31 -0300

    first commit

---
+++ js/form.js
@@ -0,0 +1,5 @@
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const signIn = document.getElementById('button');
+
